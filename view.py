865109d756e5383db9b56368db395d0c192ab4c5
varun00391 <varunsingh2191@gmail.com>
2025-05-14 18:28:30 +0530

    removed

--- view.py
+++ view.py
@@ -3,4 +3,4 @@ from utils import PodCastGenerator
 
 podcast_creator = PodCastGenerator()
 final_podcast = podcast_creator.create_podcast("The Future of AI")
-print(f"Podcast created: {final_podcast}")
+print(f"Podcast Created: {final_podcast}")
